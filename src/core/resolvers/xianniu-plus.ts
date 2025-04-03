dc9f061d966040494c7f69ed047a42b44147c85c
gaoyi <gaoyia@qq.com>
2025-04-03 15:57:27 +0800

    增加xianniu-plus

---
+++ src/core/resolvers/xianniu-plus.ts
@@ -0,0 +1,213 @@
+import type { ComponentInfo, ComponentResolver, SideEffectsInfo } from '../../types'
+import { compare } from 'compare-versions'
+import { getPkgVersion, kebabCase } from '../utils'
+
+export interface XianniuPlusResolverOptions {
+  /**
+   * import style css or sass with components
+   *
+   * @default 'css'
+   */
+  importStyle?: boolean | 'css' | 'sass'
+
+  /**
+   * use commonjs lib & source css or scss for ssr
+   */
+  ssr?: boolean
+
+  /**
+   * specify element-plus version to load style
+   *
+   * @default installed version
+   */
+  version?: string
+
+  /**
+   * auto import for directives
+   *
+   * @default true
+   */
+  directives?: boolean
+
+  /**
+   * exclude component name, if match do not resolve the name
+   */
+  exclude?: RegExp
+
+  /**
+   * a list of component names that have no styles, so resolving their styles file should be prevented
+   */
+  noStylesComponents?: string[]
+
+  /**
+   * nightly version
+   */
+  nightly?: boolean
+}
+
+type XianniuPlusResolverOptionsResolved = Required<Omit<XianniuPlusResolverOptions, 'exclude'>> &
+  Pick<XianniuPlusResolverOptions, 'exclude'>
+
+/**
+ * @deprecated
+ * @param partialName
+ * @param options
+ */
+function getSideEffectsLegacy(
+  partialName: string,
+  options: XianniuPlusResolverOptionsResolved,
+): SideEffectsInfo | undefined {
+  const { importStyle } = options
+  if (!importStyle)
+    return
+
+  if (importStyle === 'sass') {
+    return [
+      'xianniu-plus/packages/theme-chalk/src/base.scss',
+      `xianniu-plus/packages/theme-chalk/src/${partialName}.scss`,
+    ]
+  }
+  else if (importStyle === true || importStyle === 'css') {
+    return [
+      'xianniu-plus/lib/theme-chalk/base.css',
+      `xianniu-plus/lib/theme-chalk/el-${partialName}.css`,
+    ]
+  }
+}
+
+function getSideEffects(dirName: string, options: XianniuPlusResolverOptionsResolved): SideEffectsInfo | undefined {
+  const { importStyle, ssr, nightly } = options
+  const themeFolder = nightly ? '@xianniu-plus/nightly/theme-chalk' : 'xianniu-plus/theme-chalk'
+  const esComponentsFolder = nightly ? '@xianniu-plus/nightly/es/components' : 'xianniu-plus/es/components'
+
+  if (importStyle === 'sass') {
+    return ssr
+      ? [`${themeFolder}/src/base.scss`, `${themeFolder}/src/${dirName}.scss`]
+      : [`${esComponentsFolder}/base/style/index`, `${esComponentsFolder}/${dirName}/style/index`]
+  }
+  else if (importStyle === true || importStyle === 'css') {
+    return ssr
+      ? [`${themeFolder}/base.css`, `${themeFolder}/el-${dirName}.css`]
+      : [`${esComponentsFolder}/base/style/css`, `${esComponentsFolder}/${dirName}/style/css`]
+  }
+}
+
+function resolveComponent(name: string, options: XianniuPlusResolverOptionsResolved): ComponentInfo | undefined {
+  if (options.exclude && name.match(options.exclude))
+    return
+
+  if (!name.match(/^El[A-Z]/))
+    return
+
+  if (name.match(/^ElIcon.+/)) {
+    return {
+      name: name.replace(/^ElIcon/, ''),
+      from: '@xianniu-plus/icons-vue',
+    }
+  }
+
+  const partialName = kebabCase(name.slice(2))// ElTableColumn -> table-column
+  const { version, ssr, nightly } = options
+
+  // >=1.1.0-beta.1
+  if (compare(version, '1.1.0-beta.1', '>=') || nightly) {
+    return {
+      name,
+      from: `${nightly ? '@xianniu-plus/nightly' : 'xianniu-plus'}/${ssr ? 'lib' : 'es'}`,
+      sideEffects: getSideEffects(partialName, options),
+    }
+  }
+  // >=1.0.2-beta.28
+  else if (compare(version, '1.0.2-beta.28', '>=')) {
+    return {
+      from: `xianniu-plus/es/el-${partialName}`,
+      sideEffects: getSideEffectsLegacy(partialName, options),
+    }
+  }
+  // for <=1.0.1
+  else {
+    return {
+      from: `xianniu-plus/lib/el-${partialName}`,
+      sideEffects: getSideEffectsLegacy(partialName, options),
+    }
+  }
+}
+
+function resolveDirective(name: string, options: XianniuPlusResolverOptionsResolved): ComponentInfo | undefined {
+  if (!options.directives)
+    return
+
+  const directives: Record<string, { importName: string, styleName: string }> = {
+    Loading: { importName: 'ElLoadingDirective', styleName: 'loading' },
+    Popover: { importName: 'ElPopoverDirective', styleName: 'popover' },
+    InfiniteScroll: { importName: 'ElInfiniteScroll', styleName: 'infinite-scroll' },
+  }
+
+  const directive = directives[name]
+  if (!directive)
+    return
+
+  const { version, ssr, nightly } = options
+
+  // >=1.1.0-beta.1
+  if (compare(version, '1.1.0-beta.1', '>=') || nightly) {
+    return {
+      name: directive.importName,
+      from: `${nightly ? '@xianniu-plus/nightly' : 'xianniu-plus'}/${ssr ? 'lib' : 'es'}`,
+      sideEffects: getSideEffects(directive.styleName, options),
+    }
+  }
+}
+
+const noStylesComponents = ['ElAutoResizer']
+
+/**
+ * Resolver for Element Plus
+ *
+ * See https://github.com/antfu/vite-plugin-components/pull/28 for more details
+ * See https://github.com/antfu/vite-plugin-components/issues/117 for more details
+ *
+ * @author @develar @nabaonan @sxzz
+ * @link https://element-plus.org/ for element-plus
+ *
+ */
+export function XianniuPlusResolver(
+  options: XianniuPlusResolverOptions = {},
+): ComponentResolver[] {
+  let optionsResolved: XianniuPlusResolverOptionsResolved
+
+  async function resolveOptions() {
+    if (optionsResolved)
+      return optionsResolved
+    optionsResolved = {
+      ssr: false,
+      version: await getPkgVersion('xianniu-plus', '2.2.2'),
+      importStyle: 'css',
+      directives: true,
+      exclude: undefined,
+      noStylesComponents: options.noStylesComponents || [],
+      nightly: false,
+      ...options,
+    }
+    return optionsResolved
+  }
+
+  return [
+    {
+      type: 'component',
+      resolve: async (name: string) => {
+        const options = await resolveOptions()
+
+        if ([...options.noStylesComponents, ...noStylesComponents].includes(name))
+          return resolveComponent(name, { ...options, importStyle: false })
+        else return resolveComponent(name, options)
+      },
+    },
+    {
+      type: 'directive',
+      resolve: async (name: string) => {
+        return resolveDirective(name, await resolveOptions())
+      },
+    },
+  ]
+}
